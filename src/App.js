7e9d2e16b03dc843e82a723ba9087fa7421c537e
KamolineR <164529564+KamolineR@users.noreply.github.com>
2024-09-18 08:37:32 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import { BrowserRouter as Router, Routes, Rout } from "react-router-dom";
+import Header from './components/Header';
+
+import Home from './pages/Home';
+import Product from './pages/Product';
+import AddProduct from './pages/AddProduct';
+import Cart from './pages/Cart';
